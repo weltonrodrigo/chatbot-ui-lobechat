f91ab762e86d44de2e45f2cc5e5667545947aeb7
{caoyz} <28099773+CaoYunzhou@users.noreply.github.com>
2023-11-07 11:50:07 +0800

    add models

--- types/openai.ts
+++ types/openai.ts
@@ -10,17 +10,16 @@ export interface OpenAIModel {
 // Define all the model identifiers
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_POE = 'gpt-3.5-turbo-poe',
   GPT_3_5_0613 = 'gpt-3.5-turbo-0613',
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
-  GPT_3_5_16K_POE = 'gpt-3.5-turbo-16k-poe',
   GPT_3_5_16K_0613 = 'gpt-3.5-turbo-16k-0613',
+  GPT_3_5_1106 = 'gpt-3.5-turbo-1106',
   GPT_4_0613 = 'gpt-4-0613',
   GPT_4 = 'gpt-4',
-  GPT_4_POE = 'gpt-4-poe',
+  GPT_4_1106 = 'gpt-4-1106-preview',	
+  GPT_4_vision = 'gpt-4-vision-preview',	
   GPT_4_32K_0613 = 'gpt-4-32k-0613',
   GPT_4_32K = 'gpt-4-32k',
-  GPT_4_32K_POE = 'gpt-4-32k-poe',
   CLAUDE_2_100K = 'claude-2-100k',
   CLAUDE_INSTANT_100K = 'claude-instant-100k',
   CLAUDE_INSTANT = 'claude-instant',
@@ -31,19 +30,13 @@ export enum OpenAIModelID {
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_3_5_1106;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   // Define all the models with their properties
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
     name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4096,
-  },
-  [OpenAIModelID.GPT_3_5_POE]: {
-    id: OpenAIModelID.GPT_3_5_POE,
-    name: 'GPT-3.5_POE',
     maxLength: 12000,
     tokenLimit: 4096,
   },
@@ -59,15 +52,15 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 48000,
     tokenLimit: 16384,
   },
-  [OpenAIModelID.GPT_3_5_16K_POE]: {
-    id: OpenAIModelID.GPT_3_5_16K_POE,
-    name: 'GPT-3.5-16K-POE',
-    maxLength: 48000,
-    tokenLimit: 16384,
-  },
   [OpenAIModelID.GPT_3_5_16K_0613]: {
     id: OpenAIModelID.GPT_3_5_16K_0613,
     name: 'GPT-3.5-16K-0613',
+    maxLength: 48000,
+    tokenLimit: 16384,
+  },
+  [OpenAIModelID.GPT_3_5_1106]: {
+    id: OpenAIModelID.GPT_3_5_1106,
+    name: 'gpt-3.5-turbo-1106',
     maxLength: 48000,
     tokenLimit: 16384,
   },
@@ -83,11 +76,17 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 24000,
     tokenLimit: 8192,
   },
-  [OpenAIModelID.GPT_4_POE]: {
-    id: OpenAIModelID.GPT_4_POE,
-    name: 'GPT-4-POE',
-    maxLength: 24000,
-    tokenLimit: 8192,
+  [OpenAIModelID.GPT_4_1106]: {
+    id: OpenAIModelID.GPT_4_1106,
+    name: 'gpt-4-1106-preview',
+    maxLength: 376000,
+    tokenLimit: 128000,
+  },
+  [OpenAIModelID.GPT_4_vision]: {
+    id: OpenAIModelID.GPT_4_vision,
+    name: 'gpt-4-vision-preview',
+    maxLength: 376000,
+    tokenLimit: 128000,
   },
   [OpenAIModelID.GPT_4_32K_0613]: {
     id: OpenAIModelID.GPT_4_32K_0613,
@@ -98,12 +97,6 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_4_32K]: {
     id: OpenAIModelID.GPT_4_32K,
     name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32768,
-  },
-  [OpenAIModelID.GPT_4_32K_POE]: {
-    id: OpenAIModelID.GPT_4_32K_POE,
-    name: 'GPT-4-32K-POE',
     maxLength: 96000,
     tokenLimit: 32768,
   },
